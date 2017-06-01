70459fcb734c19b9e9801837252d4577f9d7aab3
Melvin <delpierremelvin@gmail.com>
2017-06-01 10:00:29 +0200

    added deplacer

--- req_jeu.js
+++ req_jeu.js
@@ -1,10 +1,10 @@
 /*
-**File name : req_jeu.js
-** Creation Date : 22-05-2017
-** Last Modified : Mon 22 May 2017 13:59:20 CEST
-** Created by : Melvin DELPIERRE
-** NOTE :
-*/
+ **File name : req_jeu.js
+ ** Creation Date : 22-05-2017
+ ** Last Modified : Thu 01 Jun 2017 10:00:09 CEST
+ ** Created by : Melvin DELPIERRE
+ ** NOTE :
+ */
 
 "use strict";
 
@@ -15,43 +15,54 @@ require('remedial');
 var jeu = function(req,res,query){
 
 
-var marqueurs = {};
-var page;
-var idx;
-var FirstD;
-var SecondD;
+	var marqueurs = {};
+	var page;
+	var idx;
+	var FirstD;
+	var SecondD;
 
-marqueurs.FirstD = "";
-marqueurs.SecondD = "";
-marqueurs.total = "";
-marqueurs.compte = query.compte;
-marqueurs.hote = query.hote;
+	marqueurs.FirstD = "";
+	marqueurs.SecondD = "";
+	marqueurs.total = "";
+	marqueurs.compte = query.compte;
+	marqueurs.hote = query.hote;
 
-if(query.action === "actif"){
+	if(query.action === "actif"){
 
-page = fs.readFileSync("plateau_actif.html", "utf-8");
+		page = fs.readFileSync("plateau_actif.html", "utf-8");
 
-}
+	}
 
-if(query.action === "passif"){
+	if(query.action === "passif"){
 
-page = fs.readFileSync("plateau_passif.html", "utf-8");
+		page = fs.readFileSync("plateau_passif.html", "utf-8");
 
-}
+	}
 
-if(query.action === "lancerD"){
+	if(query.action === "lancerD"){
 
+		marqueurs.FirstD = Number(Math.floor(Math.random()*6)+1);
+		marqueurs.SecondD = Number(Math.floor(Math.random()*6)+1);
+		marqueurs.total = marqueurs.FirstD + marqueurs.SecondD;
+		page = fs.readFileSync("plateau_deplacement.html","utf-8");
+	}
 
-marqueurs.FirstD = Number(Math.floor(Math.random()*6)+1);
-marqueurs.SecondD = Number(Math.floor(Math.random()*6)+1);
-marqueurs.total = marqueurs.FirstD + marqueurs.SecondD;
-deplacer(Number(marqueurs.total));
-page = fs.readFileSync("plateau_deplacement.html","utf-8");
-}
+	if (query.action === "deplacer"){
+		var game;
 
-page = page.supplant(marqueurs);
-res.write(page);
-res.end();
+		game = fs.readFileSync(query.hote + ".json", "UTF-8");
+		game = JSON.parse(game);
+		
+		game.position[game.actif][0] = query.x;
+		game.position[game.actif][1] = query.y;
+
+		game = JSON.stringify(game);
+		fs.writeFileSync(query.hote + ".json", game, "UTF-8");
+	}
+
+	page = page.supplant(marqueurs);
+	res.write(page);
+	res.end();
 
 };
 
